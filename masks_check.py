0977d391bea2306f0eead681c76541861e95e9e1
TapokSmerti <levgor14@gmail.com>
2026-05-30 00:43:07 +0300

    quickadd

--- masks_check.py
+++ masks_check.py
@@ -1,5 +1,7 @@
 import json
 import cv2
+import numpy as np
+
 
 img = cv2.imread("dataset/sign_dataset/train/1.jpg")
 
@@ -12,3 +14,14 @@ print("num masks:", len(d["masks"]))
 print("mask shape:", len(d["masks"][0]), len(d["masks"][0][0]))
 print("classes:", d["class_ids"])
 print("bbox:", d["bbox"][0])
+
+
+with open(
+    "dataset/sign_dataset/train/1.jpg_coco.json"
+) as f:
+    d = json.load(f)
+
+m = np.array(d["masks"])
+
+print('json data')
+print(m.shape)
